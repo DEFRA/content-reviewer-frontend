675852e7068cf0b0db22675e9841e5c7e649818f
ljohndefra <leena.john@defra.gov.uk>
2026-01-22 16:25:56 +0000

    Added default filename

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -100,7 +100,7 @@ export const homeController = {
           ...r,
           id: r.id || r.reviewId || r.jobId || r._id,
           reviewId: r.reviewId || r.id || r.jobId || r._id,
-          filename: r.fileName || r.filename || '', // map fileName to filename
+          filename: r.fileName || r.filename || 'Text Content', // map fileName to filename
           uploadedAt: r.createdAt || r.updatedAt || r.uploadedAt || null // map createdAt/updatedAt to uploadedAt
         }))
 
