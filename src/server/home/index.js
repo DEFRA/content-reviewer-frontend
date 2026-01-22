7a46861940d3fefdf312262050f6428f210ddd3f
ljohndefra <leena.john@defra.gov.uk>
2026-01-22 20:03:38 +0000

    Fixed dropdown logic

--- src/server/home/index.js
+++ src/server/home/index.js
@@ -11,7 +11,7 @@ export const home = {
       server.route([
         {
           method: 'GET',
-          path: '/',
+          path: '/api/reviews',
           ...homeController
         }
       ])
